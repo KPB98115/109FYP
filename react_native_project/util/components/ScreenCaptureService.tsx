c758cc938c05de3587a7a84eecb31c8b306e0b05
KBP98115 <kingstonsze@protonmail.com>
2023-08-03 09:25:32 +0800

    Update requirement.txt and multi-thread process
    add Yolov5 dependencies and using multi-thread to handle the work in api.py

--- react_native_project/util/components/ScreenCaptureService.tsx
+++ react_native_project/util/components/ScreenCaptureService.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import ViewShot from 'react-native-view-shot';
 import { captureScreen } from 'react-native-view-shot';
@@ -8,19 +8,19 @@ import RNFS from 'react-native-fs';
 import AuthContext from '../data/Context';
 
 const ScreenCaptureService: React.FC = () => {
-  console.log('Start captureing...');
+  console.log('Start ScreenCaptureService...');
   const camera = useRef<Camera>(null);
   const authContext = useContext(AuthContext);
   const devices = useCameraDevices();
   const device: any = devices.front;
   const captureInterval = 1000;
+  const [isLogin, setIslogin] = useState(true);
 
   const facialCapture = async () => {
     if (camera.current !== null) {
       const snapshot = await camera.current.takeSnapshot({
         quality: 100,
       });
-      console.log(snapshot.path);
       const formData = new FormData();
       formData.append('username', authContext.currentUser);
       if (snapshot.path === '') {
@@ -29,17 +29,15 @@ const ScreenCaptureService: React.FC = () => {
       await RNFS.readFile(snapshot.path, 'base64').then(base64_image => {
         formData.append('image', base64_image);
       });
-      console.log(`fetching formData with: ${authContext.currentUser}`);
-      fetch('http://172.20.10.4:5000/realtime_authentication', {
+      fetch('http://172.31.114.168:5000/realtime_authentication', {
         method: 'POST',
         headers: {'Content-Type': 'multipart/form-data'},
         body: formData,
       }).then(res => res.json()).then(json => {
-        if (json.auth) {
-          console.log('Access granted');
-        }
-        else {
-          console.error('Access declined');
+        if (!json.auth) {
+          authContext.currentUser = '';
+          setIslogin(false);
+          console.error('Access declined: Unauthorized user detected.');
         }
       }).catch(error => console.log(error));
     }
@@ -47,18 +45,19 @@ const ScreenCaptureService: React.FC = () => {
 
   const screenCapture = async () => {
     captureScreen({ format: 'jpg', quality: 1, result: 'base64' }).then(async (base64_image) => {
-      console.log(base64_image.slice(0, 10));
       const formData = new FormData();
       formData.append('screenshot', base64_image);
       formData.append('user', authContext.currentUser);
 
-      fetch('http://172.20.10.4:5000/screenshot_detection', {
+      fetch('http://172.31.114.168:5000/screenshot_detection', {
         method: 'POST',
         headers: {'Content-Type': 'multipart/form-data'},
         body: formData,
       })
       .then(res => res.json())
-      .then(result => console.log(result))
+      .then(result => {
+        authContext.pixelateArea = result;
+      })
       .catch((error) => {
         console.error('Error uploading screenshot:', error);
       });
--- rest_api/api.py
+++ rest_api/api.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import face_detect
-import object_detect
+import object_delect
 from PIL import Image
 import numpy as np
 import cv2
-
+import concurrent.futures
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    return '''<h1>Heelo world</h1>'''
+  return '''<h1>Heelo world</h1>'''
 
 @app.route('/authentication', methods=['POST'])
 def authentication():
@@ -20,20 +20,21 @@ def authentication():
     'auth': True, 'user': 'root', 'status': 'Welcome, root user'
   }
 
-@app.route('/faical_authentication', methods=['POST'])
+@app.route('/facial_authentication', methods=['POST'])
 def faical_authentication():
   # Read the image from the HTTP request
   print('Processing faical detection')
   imageData = request.form['image']
+  print(imageData[20:])
 
   try:
     # Perform facial recognition and return the result
+    # Replace the facial recognition code with your own implementation
     result = face_detect.detection(imageData)
     print(result)
     user = result[0]
     is_authenticated = result[1]
     status = result[2]
-    
     return {
         "auth": is_authenticated,
         "user": user,
@@ -47,8 +48,8 @@ def faical_authentication():
       "status": str(error)
     }
   
-@app.route('/realtime_authenticate', methods=['POST'])
-def recognition():
+@app.route('/realtime_authentication', methods=['POST'])
+def realtime_authentication():
   # Read the image from the HTTP request
   try:
     formData = request.form['image']
@@ -58,22 +59,29 @@ def recognition():
     return {'auth': False, 'status': 'formData not found'}
 
   try:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+      future = executor.submit(face_detect.realtime_detection, userID, formData)
     # Perform facial recognition and return the result as a boolean value
-    result = face_detect.realtime_detection(userID, formData)
+    result = future.result()
+    print(type(result))
     if result:
       print('Status: Access granted')
-      return {'auth': result, 'status': 'Access granted'}
-    print('Status: Access delined, ')
-    return {'auth': result, 'status': 'Access delined'}
+      return {'auth': result, 'status': ''}
+    print('Status: Access declined, ')
+    return {'auth': result, 'status': ''}
   except Exception as error:
     print(str(error))
 
 @app.route('/screenshot_detection', methods=['POST'])
-def delection():
+def screenshot_detection():
+  #userID = request.form['user']
   screenshot_in_base64 = request.form['screenshot']
-  result = object_detect.get_coordinates(screenshot_in_base64, user_level = 0)
+  with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    future = executor.submit(object_delect.get_coordinates, screenshot_in_base64, user_level = 0)
+  result = future.result()
+  print(result)
   return jsonify(result)
 
-#Run the script with $flask run -h {device ip-address}
+#Run the script with $flask run -h 172.31.114.168
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
