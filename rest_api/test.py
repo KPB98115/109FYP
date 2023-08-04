5ace0a2c7736fcb2c1e5e8ff47de8cc80e449140
KPB <79642613+KPB98115@users.noreply.github.com>
2023-08-04 22:54:07 +0800

    Update test.py

--- rest_api/test.py
+++ rest_api/test.py
@@ -3,7 +3,7 @@ import base64
 from pathlib import Path
 
 def main():
-  URL = "http://127.0.0.1:80/screenshot_detection"
+  URL = "http://localhost:80/screenshot_detection"
   #with open('base64_example.txt', 'r') as file:
   #  IMAGE_IN_BASE64 = file.read()
   with open('image/test.jpg', 'rb') as img:
