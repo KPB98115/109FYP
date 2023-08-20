37a6f69a7eb0a4b64bd4cce25ec6896d03aee78d
Yue0131 <96831959+Yue0131@users.noreply.github.com>
2023-08-20 17:07:45 +0800

    Update Yoloviolencedetection.py

--- rest_api/Yoloviolencedetection.py
+++ rest_api/Yoloviolencedetection.py
@@ -14,34 +14,6 @@ def filter_forbidden_objects(user_level, object_list):
     else:
         return []
 
-# 使用者等級，默認為0
-user_level = 0
-
-# 載入預訓練的 YOLOv5s 模型
-model = YOLO('/content/drive/MyDrive/20230714yolov5.pt')
-
-# 定義圖片檔案的路徑
-image_path = '/content/drive/MyDrive/Colab Notebooks/violencetest/violencetest4.png'
-
-# 在圖片上執行推論
-results = model.predict(image_path)  # 在圖片上執行預測
-
-# 創建一個包含被屏蔽物件座標的列表
-forbidden_object_coordinates = []
-
-# 處理預測結果
-for r in results:
-    boxes = r.boxes.xyxy.cpu().numpy()  # 取得 xyxy 格式的框線座標，並轉換為 CPU 上的 numpy 陣列
-
-    for box in boxes:
-        # 提取框線座標
-        x_min, y_min, x_max, y_max = map(int, box[:4])
-
-        # 添加框線座標到屏蔽列表中
-        forbidden_object_coordinates.append({'xmin': x_min, 'ymin': y_min, 'xmax': x_max, 'ymax': y_max})
-
-# 調用存取控制方法
-forbidden_object_indices = filter_forbidden_objects(user_level, forbidden_object_coordinates)
 
 # 將座標清單整理為整數，並作為返回值
 def get_forbidden_object_coordinates(indices):
@@ -50,9 +22,31 @@ def get_forbidden_object_coordinates(indices):
         x_min, y_min, x_max, y_max = coordinates.values()
         coordinates_list.append({'xmin': int(x_min), 'ymin': int(y_min), 'xmax': int(x_max), 'ymax': int(y_max)})
     return coordinates_list
+    
+# Default user level is 0
+def get_coordinates(base64_image, user_level = 0):
+  try:
+    # 載入預訓練的 YOLOv5s 模型
+    model = YOLO('/content/drive/MyDrive/20230714yolov5.pt')
+    # 定義圖片檔案的路徑
+    image_path = '/content/drive/MyDrive/Colab Notebooks/violencetest/violencetest4.png'
+    # 在圖片上執行推論
+    results = model.predict(image_path)  # 在圖片上執行預測
+    # 創建一個包含被屏蔽物件座標的列表
+    forbidden_object_coordinates = []
+    # 處理預測結果
+    for r in results:
+        boxes = r.boxes.xyxy.cpu().numpy()  # 取得 xyxy 格式的框線座標，並轉換為 CPU 上的 numpy 陣列
+        for box in boxes:
+            # 提取框線座標
+            x_min, y_min, x_max, y_max = map(int, box[:4])
+            # 添加框線座標到屏蔽列表中
+            forbidden_object_coordinates.append({'xmin': x_min, 'ymin': y_min, 'xmax': x_max, 'ymax': y_max})
+    # 調用存取控制方法
+    forbidden_object_indices = filter_forbidden_objects(user_level, forbidden_object_coordinates)
+    os.remove('screenshot.jpg')
+    return forbidden_object_indices
 
-# 获取整数的被屏蔽物件座標清單
-forbidden_coordinates_list = get_forbidden_object_coordinates(forbidden_object_indices)
-
-# 返回整数座標清單
-forbidden_coordinates_list
+  except Exception as error:
+    print("Failed extract coordinates:" + str(error))
+    return []
