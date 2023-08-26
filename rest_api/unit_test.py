95b7d34d31b4d7465ca6fd6d50da1d068518bb3a
KBP98115 <kingstonsze@protonmail.com>
2023-08-26 19:40:24 +0800

    Update unit_test.py
    add response time

--- rest_api/unit_test.py
+++ rest_api/unit_test.py
@@ -1,6 +1,7 @@
 import unittest
 from api import app
 import base64
+from time import time
 
 class FlaskAppTests(unittest.TestCase):
   def setUp(self):
@@ -10,32 +11,34 @@ class FlaskAppTests(unittest.TestCase):
       image = img.read()
       base64_image = base64.b64encode(image)
       self.base64_str = base64_image.decode('utf-8')
+    self.start = time()
+    self.result = []
 
   def test_authentication_response(self):
     response = self.app.post('/authentication')
 
-    print("Response Data from simple authentication:", response.get_json())
+    self.result.append("Response Data from simple authentication:", response.get_json(), "\n\tResponse time: ", self.start - time())
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content_type, 'application/json')
 
   def test_facial_authentication_response(self):
     response = self.app.post('/facial_authentication', data={'image': self.base64_str, 'username': 'kingston'}, content_type='multipart/form-data')
 
-    print("Response Data from facial authentication:", response.get_json()['status'])
+    self.result.append("Response Data from facial authentication:", response.get_json()['status'], "\n\tResponse time: ", self.start - time())
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content_type, 'application/json')
   
   def test_realTime_authentication_response(self):
     response = self.app.post('/realtime_authentication', data={'image': self.base64_str, 'username': 'kingston'}, content_type='multipart/form-data')
 
-    print("Response Data from real time authentication:", response.get_json()['auth'])
+    self.result.append("Response Data from real time authentication:", response.get_json()['auth'], "\n\tResponse time: ", self.start - time())
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content_type, 'application/json')
 
   def test_screen_detection_response(self):
     response = self.app.post('/screenshot_detection', data={'screenshot': self.base64_str, 'username': 'kingston'}, content_type='multipart/form-data')
 
-    print("Response Data from screenshot detection:", int(len(response.get_json()[0])/2), "objects detected.")
+    self.result.append("Response Data from screenshot detection:", int(len(response.get_json()[0])/2), "objects detected.", "\n\tResponse time: ", self.start - time())
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content_type, 'application/json')
 
