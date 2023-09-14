b4aae22a44969b34dafd8bf46b687fc39784f3d6
Yue0131 <96831959+Yue0131@users.noreply.github.com>
2023-09-14 22:23:10 +0800

    Update unit_test.py
    嘗試處理unexpected indent

--- rest_api/unit_test.py
+++ rest_api/unit_test.py
@@ -37,6 +37,7 @@ class FlaskAppTests(unittest.TestCase):
     self.result.append(result)
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content_type, 'application/json')
+    
 """
   def test_screen_detection_response(self):
     response = self.app.post('/screenshot_detection', data={'screenshot': self.base64_str, 'username': 'kingston'}, content_type='multipart/form-data')
@@ -46,14 +47,15 @@ class FlaskAppTests(unittest.TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content_type, 'application/json')
 """
+
   def test_yoloviolence_detection_response(self):
     response = self.app.post('/yoloviolencedetection', data={'screenshot': self.base64_str, 'username': 'kingston'}, content_type='multipart/form-data')
-
+    
     result = "Response Data from violence detection:"+ str(len(response.get_json())) + "\n\tResponse time: " + str(self.start - time())
     self.result.append(result)
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.content_type, 'application/json')
-  
+
   def show_results(self):
     for res in self.result:
       print(res)
